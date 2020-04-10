255afeeec505a3049e2945accb66f453bb3bc8bc
LaksCastro <lakhsmycastro336@gmail.com>
2020-04-10 20:19:04 -0300

    add initial copy of parallel project

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -14,7 +14,7 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, "../build"),
     compress: true,
-    port: 3000,
+    port: 5555,
     overlay: true
   },
   devtool: "cheap-module-eval-source-map",
